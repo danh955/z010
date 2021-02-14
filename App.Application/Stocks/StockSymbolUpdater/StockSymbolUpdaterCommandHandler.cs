4971c05603684aea8bbd7e152ff86e2da4e94065
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2021-02-14 14:50:50 +0900

    StockSymbolUpdater

---
+++ App.Application/Stocks/StockSymbolUpdater/StockSymbolUpdaterCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="StockSymbolUpdaterCommandHandler.cs" company="None">
+// Free and open source code.
+// </copyright>
+namespace App.Application.Stocks.StockSymbolUpdater
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+
+    /// <summary>
+    /// Command handler for updating the database symbols.
+    /// </summary>
+    public class StockSymbolUpdaterCommandHandler : IRequestHandler<StockSymbolUpdaterCommand>
+    {
+        /// <inheritdoc/>
+        public async Task<Unit> Handle(StockSymbolUpdaterCommand request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(10, cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ App.ApplicationTest/StockSymbolUpdaterTest.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="StockSymbolUpdaterTest.cs" company="None">
+// Free and open source code.
+// </copyright>
+
+namespace App.ApplicationTest
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using App.Application.Stocks.StockSymbolUpdater;
+    using Xunit;
+
+    /// <summary>
+    /// Test stock symbol updater class.
+    /// </summary>
+    public class StockSymbolUpdaterTest
+    {
+        /// <summary>
+        /// Handler test.
+        /// </summary>
+        /// <returns>Task.</returns>
+        [Fact]
+        public async Task HandlerTest()
+        {
+            //// TODO: Naming is bad.
+            var command = new StockSymbolUpdaterCommand();
+            var handler = new StockSymbolUpdaterCommandHandler();
+            await handler.Handle(command, CancellationToken.None);
+        }
+    }
+}
